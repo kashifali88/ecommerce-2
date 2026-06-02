b1f9b305e10a83032f1de79fa9fe9e72b137be23
kashifali88 <kashifzan28406@gmail.com>
2026-06-02 01:27:28 -0700

    fixed change bcrypt to bcryptjs

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,7 @@
 import { catchAsync } from "../utils/catchAsync.js";
 import { errorHandler } from "../utils/errorHandler.js";
 import User from "../models/userModel.js";
-import bcrypt from "bcrypt";
+import bcryptjs from "bcryptjs";
 import jwt from 'jsonwebtoken'
 import crypto from 'crypto';
 import nodemailer from 'nodemailer';
